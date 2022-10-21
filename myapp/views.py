46328c33448eb5d2743ab65e00e2b83bfb4096a4
AiharaAkira <89511903+AiharaAkira@users.noreply.github.com>
2022-10-21 23:26:45 +0900

    reason to use Django

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, HttpResponse
-
+import random
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome!')
-
+    return HttpResponse('Welcome!'+str(random.random()))
 
 def create(request):
     return HttpResponse('Create')
